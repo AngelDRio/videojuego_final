0aae5fc1107b3cdf8ee9c4886edefd1a4d6e3d99
anasoulgha <anas.oulghazi@educa.madrid.org>
2026-02-23 16:56:42 +0100

    Cambios menores y renombramiento de carpetas

--- UI/menu/menu.gd
+++ UI/menu/menu.gd
@@ -2,7 +2,7 @@ extends Control
 
 
 func _on_btn_start_pressed():
-	get_tree().change_scene_to_file("res://enviornment/enviornment.tscn")
+	get_tree().change_scene_to_file("res://environment/environment.tscn")
 
 
 func _on_btn_exit_pressed() -> void:
--- bonus/vida_extra.gd
+++ bonus/vida_extra.gd
@@ -1,7 +1,10 @@
 extends Area2D
 
 func _ready():
-	body_entered.connect(_on_body_entered)
+	if not body_entered.is_connected(_on_body_entered):
+		body_entered.connect(_on_body_entered)
+
+
 
 func _on_body_entered(body):
 	if body.has_method("quitar_vida"):
